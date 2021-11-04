41b9c293b7ba1249f8394714b3ea9ee337700707
akira-splatoon <sa0731.sp@gmail.com>
2021-11-05 02:56:32 +0900

    Support mention replacement send from Windows client

--- discordbot.py
+++ discordbot.py
@@ -96,12 +96,12 @@ async def on_message(message):
             for char in text:
                 if char in emoji.UNICODE_EMOJI['en'] and char in emoji_dataset:
                     text = text.replace(char, emoji_dataset[char]['short_name'])
-            pattern = r'<@(\d+)>'
+            pattern = r'<@!?(\d+)>'
             match = re.findall(pattern, text)
             for user_id in match:
                 user = await client.fetch_user(user_id)
                 user_name = f'、{user.name}へのメンション、'
-                text = re.sub(f'<@{user_id}>', user_name, text)
+                text = re.sub(rf'<@!?{user_id}>', user_name, text)
             pattern = r'<@&(\d+)>'
             match = re.findall(pattern, text)
             for role_id in match:
