c03019d998ac0654cead44a1363b1fc816869add
distalkbot <distalkbot@gmail.com>
2022-08-27 13:34:38 +0900

    Support discord.py v2.0.0

--- discordbot.py
+++ discordbot.py
@@ -13,7 +13,9 @@ prefix = os.getenv('DISCORD_BOT_PREFIX', default='🦑')
 tts_lang = os.getenv('DISCORD_BOT_LANG', default='ja-JP')
 tts_voice = os.getenv('DISCORD_BOT_VOICE', default='ja-JP-Wavenet-B')
 token = os.environ['DISCORD_BOT_TOKEN']
-client = commands.Bot(command_prefix=prefix)
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix=prefix, intents=intents)
 with open('emoji_ja.json', encoding='utf-8') as file:
     emoji_dataset = json.load(file)
 database_url = os.environ.get('DATABASE_URL')
